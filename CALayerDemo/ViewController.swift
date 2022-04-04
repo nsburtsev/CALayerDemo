cc9a45cc849aff8c5c4b4d5134582eee5847164a
Nyurgun <burtsev.ns@gmail.com>
2022-04-04 23:47:50 +0900

    Add CAGradientLayer

--- CALayerDemo/ViewController.swift
+++ CALayerDemo/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var gradientLayer: CAGradientLayer! {
+        didSet {
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+            let startColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor
+            let endColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1).cgColor
+            gradientLayer.colors = [startColor, endColor]
+//            gradientLayer.locations = [0, 0.2, 1]
+        }
+    }
+    
     @IBOutlet weak var imageView: UIImageView! {
         didSet {                //Наблюдатель свойства (устанавливает свойства для imageView)
             imageView.layer.cornerRadius = imageView.frame.size.height / 2
@@ -28,9 +39,15 @@ class ViewController: UIViewController {
         }
     }
     
+    //метод, который пересчитывает frame (указываем в каких пределах отрисовывается слой при повороте экрана)
+    override func viewDidLayoutSubviews() {
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        gradientLayer = CAGradientLayer()
+        view.layer.insertSublayer(gradientLayer, at: 0) //добавляем слой в самый низ
     }
 }
 
