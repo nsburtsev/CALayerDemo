fa8e7dc92be24f6d9eb3ba46046bfcafb4df9388
Nyurgun <burtsev.ns@gmail.com>
2022-04-04 21:06:30 +0900

    Initial commit

---
+++ CALayerDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CALayerDemo
+//
+//  Created by Нюргун on 04.03.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
---
+++ CALayerDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CALayerDemo
+//
+//  Created by Нюргун on 04.03.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
